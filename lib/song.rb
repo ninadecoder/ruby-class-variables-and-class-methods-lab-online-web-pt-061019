a6e9b1862ee665be678def91129292645bfd78ba
Nina Kazzim <kazzim@icloud.com>
2019-07-26 14:10:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ class Song
   def self.genre_count
      genre_count = {}
     @@genres.each do |genre|
-      if @@genres[genre]
+      if genre_count[genre]
         genre_count[genre] += 1
       else
         genre_count[genre] = 1
